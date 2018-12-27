7d84a7a71f90261fbb5bbab3d8b154620c0ede1e
Vrushang Desai <vrushangapple@icloud.com>
2018-12-27 10:29:15 +0530

    django portfolio

---
+++ jobs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image=models.ImageField(upload_to='images/')
+    body = models.TextField()
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
